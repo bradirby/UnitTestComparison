7740d8665dc1fe1878bcc79e280f2aee7fe388aa
Brad Irby <brad@bradirby.com>
2017-08-25 12:47:22 -0300

    Initializing repo

---
+++ NuGetMSTestProject/AttributeSupportTest.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGetMSTestProject
+{
+    [TestClass]
+    [Serializable]
+    [AttributeUsage(AttributeTargets.All)]
+    public class NugetAttributeSupportTest
+    {
+        [ContextStatic]
+        [ThreadStatic]
+        [NonSerialized]
+        private string FieldDeclaration;
+
+        [Flags]
+        enum MyEnum { }
+
+
+        [AssemblyCleanup]
+        [AssemblyInitialize]
+        [ClassCleanup]
+        [ClassInitialize]
+        [CLSCompliant(true)]
+        [CssIteration("asdf")]
+        [CssProjectStructure("asdf")]
+        [DataRow(0)]
+        [DataSource("asd")]
+        [DataTestMethod]
+        [DeploymentItem("asdf")]
+        [Description("asdf")]
+        [ExpectedException(typeof(ArgumentException))]
+        [HostType]
+        [Ignore("asdf")]
+        [LoaderOptimization(1)]
+        [MTAThread]
+        [Obsolete]
+        [Owner("asd")]
+        [ParamArray]
+        [Priority(1)]
+        [Shadowing]
+        [STAThread]
+        [TestCategory("asd")]
+        [TestCleanup]
+        [TestInitialize]
+        [TestMethod]
+        [TestProperty("propName", "propVal")]
+        [Timeout(20)]
+        [WorkItem(1)]
+        public void AttributeSupportTestMethod()
+        {
+
+        }
+
+
+    }
+
+}
---
+++ UnitTestProject1/AttrubuteSupportTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    [Serializable]
+    [AttributeUsage(AttributeTargets.All)]
+    public class NugetAttributeSupportTest
+    {
+        [ContextStatic]
+        [ThreadStatic]
+        [NonSerialized]
+        private string FieldDeclaration;
+
+        [Flags]
+        enum MyEnum { }
+
+
+        [AssemblyCleanup]
+        [AssemblyInitialize]
+        [ClassCleanup]
+        [ClassInitialize]
+        [CLSCompliant(true)]
+        [CssIteration("asdf")]
+        [CssProjectStructure("asdf")]
+        [DataRow(0)]
+        [DataSource("asd")]
+        [DataTestMethod]
+        [DeploymentItem("asdf")]
+        [Description("asdf")]
+        [ExpectedException(typeof(ArgumentException))]
+        [HostType]
+        [Ignore("asdf")]
+        [LoaderOptimization(1)]
+        [MTAThread]
+        [Obsolete]
+        [Owner("asd")]
+        [ParamArray]
+        [Priority(1)]
+        [Shadowing]
+        [STAThread]
+        [TestCategory("asd")]
+        [TestCleanup]
+        [TestInitialize]
+        [TestMethod]
+        [TestProperty("propName", "propVal")]
+        [Timeout(20)]
+        [WorkItem(1)]
+        public void AttributeSupportTestMethod()
+        {
+
+        }
+
+
+    }
+
+}
